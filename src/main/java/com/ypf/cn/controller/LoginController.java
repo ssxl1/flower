52c2aefdf0f38cdfe41750d95bc4d4f57962201f
yz <yz@DESKTOP-D7GQM28>
2019-02-21 08:35:39 +0800

    send message

--- src/main/java/com/ypf/cn/controller/LoginController.java
+++ src/main/java/com/ypf/cn/controller/LoginController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.chuanglan.sms.request.SmsSendRequest;
+import com.chuanglan.sms.response.SmsSendResponse;
+import com.chuanglan.sms.util.ChuangLanSmsUtil;
 import com.ypf.cn.domain.User;
 import com.ypf.cn.service.UserService;
 import com.ypf.cn.util.StringUtil;
@@ -49,7 +53,31 @@ public class LoginController {
 	@RequestMapping(value = "/send")
 	public Map send(@RequestParam(value = "tel", required = false) String tel, HttpServletRequest request) {
 		Map mapjson = new HashMap();
+		// 短信发送的URL 请登录zz.253.com 获取完整的URL接口信息
+		String smsSingleRequestServerUrl = "http://smssh1.253.com/msg/send/json";
+		
+		// 设置您要发送的内容：其中“【】”中括号为运营商签名符号，多签名内容前置添加提交
+		String msg = "【253云通讯科技】你好,你的验证码是";
+		// 手机号码
+		// 状态报告
+		String report = "true";
+		
+		msg+=(int)((Math.random()*9+1)*100000);
+		msg+="";
+		System.out.println(msg);
+		SmsSendRequest smsSingleRequest = new SmsSendRequest(account, pswd, msg, tel, report);
 
+		String requestJson = JSON.toJSONString(smsSingleRequest);
+
+		System.out.println("before request string is: " + requestJson);
+
+		String response = ChuangLanSmsUtil.sendSmsByPost(smsSingleRequestServerUrl, requestJson);
+
+		System.out.println("response after request result is :" + response);
+
+		SmsSendResponse smsSingleResponse = JSON.parseObject(response, SmsSendResponse.class);
+
+		System.out.println("response  toString is :" + smsSingleResponse);
 		return mapjson;
 	}
 }
